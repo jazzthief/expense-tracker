309e27437c98e7cfa8ddded90dbeb543806aad13
jazzthief <mynameisyegor@gmail.com>
2022-06-09 14:37:53 +0300

    Move some styling to `Card` component

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,9 +1,10 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import "./Expenses.css"
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {props.items.map(item => {
         return <ExpenseItem
           title={item.title}
@@ -11,7 +12,7 @@ function Expenses(props) {
           amount={item.amount}
         />
       })}
-    </div>
+    </Card>
   );
 }
 
